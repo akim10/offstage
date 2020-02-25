ce14a6e22ad270487c002c54c43f487125e911f0
Andrew Kim <akim1@andrew.cmu.edu>
2020-02-24 20:31:23 -0500

    fixed domain in app.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :domain               => 'https://www.recordstage.com',
+   :domain               => 'www.recordstage.com',
    :user_name            => ENV['GMAIL_USERNAME'],
    :password             => ENV['GMAIL_PASSWORD'],
    :authentication       => :plain,
